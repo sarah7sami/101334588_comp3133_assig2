417d2510739963367b452389126a6a299494f96f
Sarah Sami <sarah7sami@gmail.com>
2023-04-08 19:09:02 -0400

    Add and implement login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { SignupComponent } from './signup/signup.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignupComponent
+    SignupComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Apollo } from 'apollo-angular';
+import { Router } from '@angular/router';
+import gql from 'graphql-tag';
+
+const LOGIN_QUERY = gql`
+  query Login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      status
+      user {
+        id
+        username
+        email
+      }
+      message
+    }
+  }
+`;
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  errorMessage: string;
+  username: string;
+  password: string;
+
+  constructor(private apollo: Apollo, private router: Router) { 
+    this.username = '';
+    this.password = '';
+    this.errorMessage = '';
+  }
+
+  ngOnInit() {
+  }
+
+  onSubmit(form: NgForm) {
+    const username = form.value.username;
+    const password = form.value.password;
+  
+    this.apollo.watchQuery<any>({
+      query: LOGIN_QUERY,
+      variables: {
+        username: username,
+        password: password
+      }
+    } as any).valueChanges.subscribe(({data}) => {
+      const loginResponse = data.login;
+  
+      if (loginResponse.status) {
+        localStorage.setItem('currentUser', JSON.stringify(loginResponse.user));
+        this.router.navigate(['/dashboard']);
+      } else {
+        this.errorMessage = loginResponse.message;
+      }
+    });
+  }  
+}
