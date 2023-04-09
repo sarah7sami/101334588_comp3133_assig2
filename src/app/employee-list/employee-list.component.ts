2ea8f02590bc427585508d761a1eae4aa94e54f6
Sarah Sami <sarah7sami@gmail.com>
2023-04-09 11:11:53 -0400

    Added delete employee - auth still has to be implemented

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -35,4 +35,23 @@ export class EmployeeListComponent implements OnInit {
         this.employees = employees;
       });
   }
+
+  deleteEmployee(id: string) {
+    if (confirm("Are you sure you want to delete this employee?")) {
+      this.apollo
+        .mutate<any>({
+          mutation: gql`
+            mutation {
+              deleteEmployee(id: "${id}") {
+                id
+              }
+            }
+          `,
+        })
+        .subscribe(({ data }) => {
+          this.employees = this.employees.filter((employee) => employee._id !== id);
+          console.log("Employee deleted successfully", data);
+        });
+    }
+  }
 }
